b9fae822f5fd1ec3cb29711748facaad1d8468b0
bipinjoshi <bipinjoshicr712@gmail.com>
2024-02-11 23:08:03 +0530

    githubUser setup done

--- src/component/GitHubUser.js/GitHunUser.js
+++ src/component/GitHubUser.js/GitHunUser.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const GitHunUser = () => {
-  return <div></div>;
+  const accessToken = "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";
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/bipin`);
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchdata();
+  });
+  return (
+    <div className="github-user-card">
+      <img src="" className="user-avatar" />
+      <div className="user-info">
+        <h2>name</h2>
+        <p>Username: </p>
+        <p>Number of Repositories: </p>
+      </div>
+    </div>
+  );
 };
 
 export default GitHunUser;
